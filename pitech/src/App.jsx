2ba95853be368a73b0d3a0f78c01e9266cbad706
Juan <helpeshs@gmail.com>
2024-04-23 00:47:34 -0300

    tela login/registrar e router

--- pitech/src/App.jsx
+++ pitech/src/App.jsx
@@ -1,20 +1,30 @@
-import './App.css'
-import CardItem from './components/layouts/CardItem'
-import CardMenu from './components/layouts/CardMenu'
-import Carousel from './components/layouts/carousel'
-import Footer from './components/layouts/footer'
-import Header from './components/layouts/header'
-import Home from './components/pages/Home/Home'
+import "./App.css";
+import CardItem from "./components/layouts/CardItem";
+import CardMenu from "./components/layouts/CardMenu";
+import Carousel from "./components/layouts/carousel";
+
+import Footer from "./components/layouts/footer";
+import Header from "./components/layouts/header";
+import Home from "./components/pages/Home/Home";
+import Login from "./components/pages/Login/Login";
+import Registrar from "./components/pages/Registrar/Registrar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <Header/>
-    <Home/>
-    <Footer/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registrar" element={<Registrar />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
-};
+}
 
 export default App;
